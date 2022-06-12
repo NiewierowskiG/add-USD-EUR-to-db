14e6c094e9f6f4eae581194fd14f7439f93abbe1
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-06-12 19:19:33 +0200

    logger

--- NBPApi.py
+++ NBPApi.py
@@ -1,9 +1,19 @@
 import requests
 
 
+class NBPApiError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.message
+
+
 def get_usd_eur_exchange_rate():
-    response = requests.get('http://api.nbp.pl/api/exchangerates/rates/a/eur/').json()
-    eur = response['rates'][0]['mid']
-    response = requests.get('http://api.nbp.pl/api/exchangerates/rates/a/usd/').json()
-    usd = response['rates'][0]['mid']
+    response1 = requests.get('http://api.nbp.pl/api/exchangerates/rates/a/eur/')
+    response2 = requests.get('http://api.nbp.pl/api/exchangerates/rates/a/usd/')
+    if response2.status_code != 200 or response1.status_code != 200:
+        raise NBPApiError("Problem with connecting to NBP api servers")
+    eur = response1.json()['rates'][0]['mid']
+    usd = response2.json()['rates'][0]['mid']
     return {"EUR": eur, "USD": usd}
--- add_prices.py
+++ add_prices.py
@@ -1,7 +1,11 @@
 import mysql.connector
 import pandas as pd
 import NBPApi
-import openpyxl
+import logging
+import os
+
+current_filename = os.path.basename(__file__).rsplit('.', 1)[0]
+logging.basicConfig(filename=current_filename + '.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 class AddPrices():
@@ -12,41 +16,59 @@ class AddPrices():
         self.database = database
 
     def connect(self):
-        return mysql.connector.connect(
+        try:
+            return mysql.connector.connect(
                 host=self.host,
                 user=self.login,
                 password=self.password,
                 database=self.database
-        )
+            )
+        except mysql.connector.errors.ProgrammingError as e:
+            logging.error(e.msg)
+        except mysql.connector.errors.DatabaseError as e:
+            logging.error(e.msg)
 
     def update(self):
         db = self.connect()
         cursor = db.cursor()
-        #alter_command = "ALTER TABLE Product ADD UnitPriceUSD DECIMAL NOT NULL ; " \
-        #                "ALTER TABLE Product ADD UnitPriceEuro DECIMAL NOT NULL ; "
         try:
             alter_command = "select UnitPriceEuro, UnitPriceEuro from Product"
             cursor.execute(alter_command)
         except mysql.connector.errors.ProgrammingError as e:
             if "Unknown column " in e.msg:
-                alter_command = "ALTER TABLE Product ADD UnitPriceUSD DECIMAL NOT NULL ; " \
-                                "ALTER TABLE Product ADD UnitPriceEuro DECIMAL NOT NULL ; "
+                alter_command = "ALTER TABLE Product ADD UnitPriceUSD DECIMAL NOT NULL;" \
+                                "ALTER TABLE Product ADD UnitPriceEuro DECIMAL NOT NULL;"
                 cursor.execute(alter_command)
+                logging.info(alter_command.split(';')[0])
+                logging.info(alter_command.split(';')[1])
+
         db = self.connect()
         cursor = db.cursor()
         df = pd.read_sql('select ProductID, UnitPrice from Product', con=db)
         update_rows = []
-        exchange_rates = NBPApi.get_usd_eur_exchange_rate()
+        try:
+            exchange_rates = NBPApi.get_usd_eur_exchange_rate()
+        except NBPApi.NBPApiError as e:
+            logging.error(e.msg)
         for i in df.iterrows():
-            update_rows.append((round(i[1]['UnitPrice'] / exchange_rates["EUR"]),
+            update_rows.append((round(i[1]['UnitPrice'] / exchange_rates['EUR']),
                                round(i[1]['UnitPrice'] / exchange_rates["USD"]),
                                i[1]['ProductID']
                                ))
-        cursor.executemany("Update Product Set UnitPriceEuro = %s, UnitPriceUSD = %s where ProductID = %s", update_rows)
-        db.commit()
+        try:
+            cursor.executemany("Update Product Set UnitPriceEuro = %s, UnitPriceUSD = %s where ProductID = %s", update_rows)
+            db.commit()
+            for row in update_rows:
+                logging.info(f"Update Product Set UnitPriceEuro = {row[0]}, UnitPriceUSD = {row[1]} where ProductID = {row[2]}")
+        except mysql.connector.errors.ProgrammingError as e:
+            logging.error(e.msg)
+        except mysql.connector.errors.DatabaseError as e:
+            logging.error(e.msg)
+
 
     def to_excel(self):
         db = self.connect()
         df = pd.read_sql('select ProductID, DepartmentID, Category, IDSKU, ProductName, Quantity, UnitPrice, UnitPriceUSD,'
                          ' UnitPriceEuro, Ranking, ProductDesc, UnitsInStock, UnitsInOrder from Product', con=db)
+
         df.to_excel("output.xlsx")
